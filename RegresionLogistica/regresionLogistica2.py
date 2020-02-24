05e858e775faf683597629e08d27fec23b36ac3b
Alejandro Cordon Urena <cordon.alejandro@gmail.com>
2020-02-24 20:26:32 +0100

    import sklearn

---
+++ RegresionLogistica/regresionLogistica2.py
@@ -0,0 +1,111 @@
+import numpy as np
+from pandas.io.parsers import read_csv
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+import sklearn as skl
+
+
+
+
+
+
+def carga_csv(file_name):
+    """carga el fichero csv especificado y lo devuelve en un array de numpy"""
+    valores = read_csv(file_name, header=None).values
+    # suponemos que siempre trabajaremos con float
+    return valores.astype(float)
+
+
+
+def sigmoid(x):
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+
+def pinta_frontera_recta(X, Y, theta):
+    #plt.figure()
+    pos = np.where(Y == 1)
+    plt.scatter(X[pos, 1], X[pos, 2], marker='+', c='k')
+    pos = np.where(Y == 0)
+    plt.scatter(X[pos, 1], X[pos, 2], marker='o', c='y')
+
+    x1_min, x1_max = X[:, 1].min(), X[:, 1].max()
+    x2_min, x2_max = X[:, 2].min(), X[:, 2].max()
+
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
+                           np.linspace(x2_min, x2_max))
+
+    h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
+                      xx1.ravel(),
+                      xx2.ravel()].dot(theta))
+    h = h.reshape(xx1.shape)
+
+    # el cuarto parámetro es el valor de z cuya frontera se
+    # quiere pintar
+    plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
+    #plt.savefig("frontera.png")
+    plt.show()
+    plt.clf()
+    #plt.close()
+
+
+
+def visualizacionDatos(X, Y):
+    pos = np.where(Y == 1)
+    plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='k')
+    pos = np.where(Y == 0)
+    plt.scatter(X[pos, 0], X[pos, 1], marker='o', c='y')
+    plt.show()
+    plt.clf()
+
+
+def cost(theta, X, Y):
+    # H = sigmoid(np.matmul(X, np.transpose(theta)))
+    H = sigmoid(np.matmul(X, theta))
+    # cost = (- 1 / (len(X))) * np.sum( Y * np.log(H) + (1 - Y) * np.log(1 - H))
+    cost = (- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
+    print('coste:',cost)
+    return cost
+
+
+def gradient(theta, XX, Y):
+    H = sigmoid(np.matmul(XX, theta))
+    grad = (1 / len(Y)) * np.matmul(XX.T, H - Y)
+    print('gradiente:',grad)
+    return grad
+
+
+def plot_decisionboundary(X, Y, theta, poly):
+    plt.figure()
+    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
+    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
+    np.linspace(x2_min, x2_max))
+    h = sigmoid(poly.fit_transform(np.c_[xx1.ravel(),
+    xx2.ravel()]).dot(theta))
+    h = h.reshape(xx1.shape)
+    plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
+    plt.savefig("boundary.png")
+    plt.close()
+
+
+def main():
+    datos = carga_csv('ex2data1.csv')
+    X = datos[:, :-1]
+    Y = datos[:, -1]
+    #visualizacionDatos(X, Y)
+
+    m = np.shape(X)[0]
+    # añadimos una columna de 1's a la X
+    X = np.hstack([np.ones([m, 1]), X])
+    #Theta = np.array([0., 0., 0.])
+    Theta = np.zeros(3)
+    result = opt.fmin_tnc(func=cost, x0=Theta, fprime=gradient, args=(X, Y))
+    theta_opt = result[0]
+    print('result:', result)
+    print('theta_opt:', theta_opt)
+    pinta_frontera_recta(X, Y, theta_opt)
+
+
+main()
+print('terminado')
