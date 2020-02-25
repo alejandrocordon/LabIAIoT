3a58e99e35f1ea64d1e812e1692e9bc43224312f
oolaya1815 <57266283+oolaya1815@users.noreply.github.com>
2020-02-25 17:06:07 +0100

    funcion de # aprobados
    Esta función devuelve la cantidad de aprobados y desaprobados de un conjunto de datos, en este caso se debe de agreagar los 1's a la matriz X, no hacer si se ha hecho, tambien debe de agregarse las thetas optimas calculadas

--- RegresionLogistica/evalregresion.py
+++ RegresionLogistica/evalregresion.py
@@ -1,6 +1,6 @@
     def prueba(x,theta)
       H = sigmoide(np.dot(X, Theta))
       desa = (H < 0.5)
-      desaprobados = len(H(desa))
+      desaprobados = len(H[desa])
       aprobados = len(H) - desaprobados
       return aprobados, desaprobados
