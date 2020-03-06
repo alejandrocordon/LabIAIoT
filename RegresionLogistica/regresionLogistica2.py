2f9fe12ac5fba45cfec02c3d1e4d58d4d3454ae7
Alejandro Cordon Urena <cordon.alejandro@gmail.com>
2020-03-06 23:58:13 +0100

    nueva funcion de coste

--- RegresionLogistica/regresionLogistica2.py
+++ RegresionLogistica/regresionLogistica2.py
@@ -1,12 +1,9 @@
 import numpy as np
 import sklearn
+from sklearn.preprocessing import PolynomialFeatures
 from pandas.io.parsers import read_csv
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
-
-
-
-
 
 
 def carga_csv(file_name):
@@ -16,14 +13,13 @@ def carga_csv(file_name):
     return valores.astype(float)
 
 
-
 def sigmoid(x):
     s = 1 / (1 + np.exp(-x))
     return s
 
 
 def pinta_frontera_recta(X, Y, theta):
-    #plt.figure()
+    # plt.figure()
     pos = np.where(Y == 1)
     plt.scatter(X[pos, 1], X[pos, 2], marker='+', c='k')
     pos = np.where(Y == 0)
@@ -35,21 +31,18 @@ def pinta_frontera_recta(X, Y, theta):
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
                            np.linspace(x2_min, x2_max))
 
-    #__init__(self, degree=2, interaction_only=False, include_bias=True,
-    h = sklearn.preprocessing.PolynomialFeatures(degree=6, interaction_only=False, include_bias=True)
-    #h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
-     #                 xx1.ravel(),
-      #                xx2.ravel()].dot(theta))
+    h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
+                      xx1.ravel(),
+                      xx2.ravel()].dot(theta))
     h = h.reshape(xx1.shape)
 
     # el cuarto parámetro es el valor de z cuya frontera se
     # quiere pintar
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
-    #plt.savefig("frontera.png")
+    # plt.savefig("frontera.png")
     plt.show()
     plt.clf()
-    #plt.close()
-
+    # plt.close()
 
 
 def visualizacionDatos(X, Y):
@@ -64,16 +57,24 @@ def visualizacionDatos(X, Y):
 def cost(theta, X, Y):
     # H = sigmoid(np.matmul(X, np.transpose(theta)))
     H = sigmoid(np.matmul(X, theta))
-    # cost = (- 1 / (len(X))) * np.sum( Y * np.log(H) + (1 - Y) * np.log(1 - H))
-    cost = (- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
-    print('coste:',cost)
+    cost = (- 1 / (len(X))) * np.sum(Y * np.log(H) + (1 - Y) * np.log(1 - H))
+    # cost = (- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
+    print('coste:', cost)
     return cost
+
+
+def coste2(x, theta):
+    H = sigmoid(np.dot(x, theta))
+    desa = (H < 0.5)
+    desaprobados = len(H[desa])
+    aprobados = len(H) - desaprobados
+    return aprobados, desaprobados
 
 
 def gradient(theta, XX, Y):
     H = sigmoid(np.matmul(XX, theta))
     grad = (1 / len(Y)) * np.matmul(XX.T, H - Y)
-    print('gradiente:',grad)
+    print('gradiente:', grad)
     return grad
 
 
@@ -81,35 +82,44 @@ def plot_decisionboundary(X, Y, theta, poly):
     plt.figure()
     x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
     x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
-    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
-    np.linspace(x2_min, x2_max))
-    h = sigmoid(poly.fit_transform(np.c_[xx1.ravel(),
-    xx2.ravel()]).dot(theta))
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
+    h = sigmoid(poly.fit_transform(np.c_[xx1.ravel(), xx2.ravel()]).dot(theta))
     h = h.reshape(xx1.shape)
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
-    plt.savefig("boundary.png")
+    plt.savefig("boundary2.png")
     plt.close()
+
+
+def prueba(x, theta):
+    H = sigmoid(np.dot(x, theta))
+    desa = (H < 0.5)
+    desaprobados = len(H[desa])
+    aprobados = len(H) - desaprobados
+    return aprobados, desaprobados
 
 
 def main():
     datos = carga_csv('ex2data2.csv')
     X = datos[:, :-1]
     Y = datos[:, -1]
-    #visualizacionDatos(X, Y)
+    # visualizacionDatos(X, Y)
 
     m = np.shape(X)[0]
     # añadimos una columna de 1's a la X
     X = np.hstack([np.ones([m, 1]), X])
-    #Theta = np.array([0., 0., 0.])
+    # Theta = np.array([0., 0., 0.])
+    # Theta = np.zeros(3)
     Theta = np.zeros(3)
     result = opt.fmin_tnc(func=cost, x0=Theta, fprime=gradient, args=(X, Y))
 
-    #result = sklearn.preprocessing.PolynomialFeatures()
+    #poly = sklearn.preprocessing.PolynomialFeatures(degree=6, interaction_only=False, include_bias=True)
 
     theta_opt = result[0]
-    print('result:', result)
-    print('theta_opt:', theta_opt)
-    pinta_frontera_recta(X, Y, theta_opt)
+    # print('result:', result)
+    # print('theta_opt:', theta_opt)
+    # pinta_frontera_recta(X, Y, theta_opt)
+
+    plot_decisionboundary(X, Y, theta_opt, result)
 
 
 main()
